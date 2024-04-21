30f39d89c05a4bf3fe74d81adb1cdf34a15c9dda
Nikola Kuburović <nikola.kuburovic123@gmail.com>
2024-04-21 18:15:45 +0200

    refactor: Use CSS to style the grid and snake

--- ui.py
+++ ui.py
@@ -1,42 +1,61 @@
 #!/usr/bin/env python3
 import sys
-from PySide6 import QtWidgets
-from PySide6.QtWidgets import QWidget, QMainWindow, QGridLayout
+
+# from PySide6 import QtWidgets, QtCore
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QGridLayout, QLabel
 from PySide6.QtGui import QColor, QPalette
 
-class Color(QWidget):
-    def __init__(self, color):
-        super(Color, self).__init__()
-        self.setAutoFillBackground(True)
 
-        palette = self.palette()
-        palette.setColor(QPalette.ColorRole.Window, QColor(color))
-        self.setPalette(palette)
+# TODO: This should be moved probably
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
 
+    def __eq__(self, other):
+        return isinstance(other, Point) and self.x == other.x and self.y == other.y
 
 
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.num_rows = 10;
-        self.num_columns = 10;
+        self.num_rows = 10
+        self.num_columns = 10
 
-        layout = QGridLayout();
-        for i in range(0, self.num_rows):
-            for j in range(0, self.num_columns):
-                layout.addWidget(Color('gray'), i, j);
+        snake = [Point(2, 2), Point(2, 3), Point(2, 4)]
+        layout = self.initialize_grid(snake, 10, 10, "gray")
 
-        widget = QWidget();
-        widget.setLayout(layout);
-        self.setCentralWidget(widget);
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
 
+    def initialize_grid(
+        self,
+        snake: list[Point],  # TODO: class Snake()...
+        n_rows: int,
+        n_columns: int,
+        grid_color: str,
+    ) -> QGridLayout:
+        layout = QGridLayout()
+        print("Snek:", snake)
+        for i in range(0, n_rows):
+            for j in range(0, n_columns):
+                label = QLabel()
+                if Point(i, j) in snake:
+                    label.setStyleSheet("background-color: green")
+                    layout.addWidget(label, i, j)
+                else:
+                    label.setStyleSheet(f"background-color: {grid_color}")
+                    layout.addWidget(label, i, j)
+        return layout
 
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QApplication([])
 
-    window = MainWindow();
+    window = MainWindow()
     window.show()
 
     sys.exit(app.exec())
