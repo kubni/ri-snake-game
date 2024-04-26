6b6c6a71ed4e19af60a765f9a826d470abb031f9
Nikola Kuburović <nikola.kuburovic123@gmail.com>
2024-04-26 12:15:09 +0200

    refactor: Improve get_best_individual_and_fitness

--- ga.py
+++ ga.py
@@ -15,14 +15,8 @@ class Population:
             self.population.append(snake)
 
     def get_best_individual_and_fitness(self) -> Tuple[Snake, float]:
-        best_fitness = float("-inf")
-        best_snake = self.population[0]
-        for s in self.population:
-            if s.fitness > best_fitness:
-                best_fitness = s.fitness
-                best_snake = s
-
-        return (best_snake, best_fitness)
+        best_snake = max(self.population, key=lambda s: s.fitness)
+        return (best_snake, best_snake.fitness)
 
     def calculate_fitness(self):
         for s in self.population:
